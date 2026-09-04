b2b5e74dd4597f6c222ba2fef3f827c12462c623
Amisha Praharaj <amishapraharaj2@gmail.com>
2026-09-05 00:42:41 +0530

    Update base URL to use environment variable

--- client/src/store/api/apiSlice.js
+++ client/src/store/api/apiSlice.js
@@ -7,7 +7,7 @@ export const apiSlice = createApi({
   reducerPath: "api",
 
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: import.meta.env.VITE_API_URL,
 
     credentials: "include",
   }),
